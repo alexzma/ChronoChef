a977c1b376e2564b5eb11d967433a4ffa1fdf792
BuildTools <unconfigured@null.spigotmc.org>
2020-09-05 16:50:26 -0700

    add pick up mechanics in terms of direction

---
+++ Assets/SceneAssets/Betto_Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+enum Direction
+{
+    Up,
+    Down,
+    Left,
+    Right
+};
+
+// An interface added to the main class
+interface IPlayerInteraction
+{
+
+    // Pick up the item in front of the tile direction. Returns the item in front (probably the object)
+    string PickUp(Vector3 position, Direction direction);
+
+    // Drop the item in the next tile in the specified direction. Returns whether it was succesful or not.
+    bool DropItem(Vector3 position, string item, Direction direction);
+
+    // Launch chrono-bomb in the specified direction.
+    void ThrowChronoBomb(Vector3 position, Direction direction);
+}
