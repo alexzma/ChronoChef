cc4ad987004b8f1599ddb93e9ebef8c2f23f19b1
Kaustuv Prateeq <kaustuvprateeq200@gmail.com>
2020-09-05 15:52:20 -0700

    CC3KP Added Sprite

---
+++ Assets/Player_movement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_movement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
