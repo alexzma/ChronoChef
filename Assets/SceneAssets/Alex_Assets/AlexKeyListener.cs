d482f1d83321b8e6fb34b9f204aaef790cee740e
Alexander Ma <alexanderzma@hotmail.com>
2020-09-05 19:59:40 -0700

    Added base for a menu

--- Assets/SceneAssets/Alex_Assets/AlexKeyListener.cs
+++ Assets/SceneAssets/Alex_Assets/AlexKeyListener.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class AlexKeyListener : MonoBehaviour
 {
+    public GameObject menu;
     public GameObject inventory;
     private bool inventory_active = false;
+    private bool menu_active = false;
 
     struct InventoryAction
     {
@@ -38,6 +40,12 @@ public class AlexKeyListener : MonoBehaviour
                 }
             }
             inventoryQueue = new List<InventoryAction>();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            menu.SetActive(!menu_active);
+            menu_active = !menu_active;
         }
         if (Input.GetKeyDown(KeyCode.I))
         {
