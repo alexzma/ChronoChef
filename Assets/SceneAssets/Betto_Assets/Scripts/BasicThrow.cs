dc5a8d1fc93ba4002613bf2e3891024c171c5d3c
BuildTools <unconfigured@null.spigotmc.org>
2020-09-06 12:10:41 -0700

    Add time state with bomb collision

---
+++ Assets/SceneAssets/Betto_Assets/Scripts/BasicThrow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicThrow : MonoBehaviour
+{
+    public Transform bomb;
+    private void Update()
+    {
+        Instantiate(bomb);
+    }
+}
