b04a69c3f32d7213c7266e2124ce83cdaa2723e1
Kaustuv Prateeq <kaustuvprateeq200@gmail.com>
2020-09-06 10:04:28 -0700

    CC6KP Added On Collision Script
    Unfinished Collision script that spawns "past/Future" object at the object location

---
+++ Assets/KP_on_bomb_collision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KP_on_collision : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    //attached to 
+    void OnCollisionEnter(Collision other)
+    {
+        if(gameObject.tag == "redBomb" && 
+            other.gameObject.tag == "item" )
+        {
+            Destroy(gameObject);
+            Destroy(other);
+            //spawn future instance of gameObject
+        }
+
+        if (gameObject.tag == "blueBomb" &&
+             other.gameObject.tag == "item")
+        {
+            Destroy(gameObject);
+            Destroy(other);
+            //spawn past instance of gameObject
+        }
+    }
+}
