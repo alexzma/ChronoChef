c75a8cc4a82c84dfebdfda899d46cb9077d86229
BuildTools <unconfigured@null.spigotmc.org>
2020-09-06 17:32:28 -0700

    Add per-room display of ingredients

--- Assets/Scripts/Alex/IngredientTracker.cs
+++ Assets/Scripts/Alex/IngredientTracker.cs
@@ -1,11 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IngredientTracker : MonoBehaviour
 {
     public GameObject foodItem;
     public GameObject ingredientList;
+    // Required ingredients for all the rooms
+    public RoomRequiredIngredients[] requiredIngredientNamesExt;
+    public string initialRoom;
+
+    private Dictionary<string, List<Ingredient>> roomToRequiredIngredients;
+    private Dictionary<string, RectTransform> roomToListTransform;
 
     private struct Ingredient
     {
@@ -14,19 +23,44 @@ public class IngredientTracker : MonoBehaviour
         public string name;
     }
 
+    // The only reason for this class is to display the Dictionary easily in the inspector for easy editing.
+    [Serializable]
+    public struct RoomRequiredIngredients
+    {
+        public string room;
+        public string[] ingredientNames;
+    }
+
     private List<Ingredient> ingredients;
+    private string activeRoom;
+
+    private void Awake()
+    {
+        roomToRequiredIngredients = new Dictionary<string, List<Ingredient>>();
+        roomToListTransform = new Dictionary<string, RectTransform>();
+        activeRoom = initialRoom;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         ingredients = new List<Ingredient>();
 
+        foreach (RoomRequiredIngredients reqIngredient in requiredIngredientNamesExt)
+        {
+            foreach (string ingredientName in reqIngredient.ingredientNames)
+            {
+                AddIngredient(reqIngredient.room, ingredientName);
+            }
+        }
+
         //Test add
-        AddIngredient("Bonito Flakes");
-        AddIngredient("Egg");
-        AddIngredient("Green Onions");
-        AddIngredient("Bamboo Shoots");
-        AddIngredient("Pork Belly");
+        //AddIngredient("", "Bonito Flakes");
+        //AddIngredient("", "Egg");
+        //AddIngredient("", "Green Onions");
+        //AddIngredient("", "Bamboo Shoots");
+        //AddIngredient("", "Pork Belly");
+        DisplayRoom(activeRoom); // Test, would usually call this from an outside class obtaining this tracker component
 
         //Test verify
         VerifyIngredient("Pork Belly");
@@ -43,10 +77,21 @@ public class IngredientTracker : MonoBehaviour
     void Update()
     {}
 
-    public void AddIngredient(string name)
+    public void AddIngredient(string room, string name)
     {
+        if (!roomToListTransform.ContainsKey(room))
+        {
+            GameObject obj = new GameObject();
+            GameObject newObj = Instantiate(obj, ingredientList.transform);
+            newObj.AddComponent<VerticalLayoutGroup>();
+            newObj.AddComponent<RectTransform>();
+            newObj.transform.parent = transform;
+            newObj.SetActive(false);
+            roomToListTransform.Add(room, newObj.GetComponent<RectTransform>());
+        }
+        Transform parentTransform = roomToListTransform[room];
         //create object
-        GameObject gameObject = Instantiate(foodItem, ingredientList.transform);
+        GameObject gameObject = Instantiate(foodItem, parentTransform);
 
         //change the name of the created object
         FoodItemManager foodItemManager = gameObject.GetComponent<FoodItemManager>();
@@ -58,6 +103,11 @@ public class IngredientTracker : MonoBehaviour
         ingredient.status = false;
         ingredient.name = name;
         ingredients.Add(ingredient);
+        if (!roomToRequiredIngredients.ContainsKey(room))
+        {
+            roomToRequiredIngredients.Add(room, new List<Ingredient>());
+        }
+        roomToRequiredIngredients[room].Add(ingredient);
     }
 
     public void VerifyIngredient(string name)
@@ -91,5 +141,29 @@ public class IngredientTracker : MonoBehaviour
         }
         Debug.Log("Ingredient not found: " + name);
         return false;
+    }
+
+    public void DisplayRoom(string room)
+    {
+        if (roomToListTransform.ContainsKey(room))
+        {
+            Debug.Log(activeRoom);
+            roomToListTransform[activeRoom].gameObject.SetActive(false);
+            roomToListTransform[room].gameObject.SetActive(true);
+            Debug.Log("enabled");
+        }
+    }
+
+    public float CalculatePercentageDone()
+    {
+        int amountVerified = 0;
+        foreach(Ingredient ingredient in ingredients) {
+            if (ingredient.status)
+            {
+                // Is verified
+                amountVerified++;
+            }
+        }
+        return (float)amountVerified / ingredients.Count;
     }
 }
---
+++ Assets/Scripts/Betto/EvaluateLevelScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EvaluateLevelScript : MonoBehaviour
+{
+    [SerializeField] private IngredientTracker ingredients;
+    [SerializeField] private float targetPercentage;
+    [SerializeField] private string winScene;
+    [SerializeField] private string lossScene;
+
+    public void EvaluateScore()
+    {
+        if (ingredients.CalculatePercentageDone() >= targetPercentage)
+        {
+            SceneManager.LoadScene(winScene);
+        } else
+        {
+            SceneManager.LoadScene(lossScene);
+        }
+    }
+}
