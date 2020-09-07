1d57e1f2b563b8d68493657f76bd9a4768572737
markgue <32917194+markgue@users.noreply.github.com>
2020-09-06 18:43:06 -0700

    Somewhat working flowing lava

---
+++ Assets/Scripts/Mark/LavaFlow.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UI;
+using System;
+
+public class LavaFlow : MonoBehaviour
+{
+    public float tickRate = 1;
+    public Tilemap tilemap;
+    public RuleTile lavaTile;
+    public BoundsInt bounds;
+
+    private Vector3Int pos;
+    private float clockTick;
+
+    private void Start()
+    {
+        clockTick = Time.time + tickRate;      
+    }
+
+    void Update()
+    {
+        
+        if (Time.time > clockTick)
+        {
+            clockTick += tickRate;
+            TileBase[] tileArray = tilemap.GetTilesBlock(bounds);
+
+            for (int x = 0; x < bounds.size.x; x++)
+            {
+                for (int y = 0; y < bounds.size.y; y++)
+                {
+                    TileBase tile = tileArray[x + y * bounds.size.x];
+                    pos.x = bounds.position.x + x;
+                    pos.y = bounds.position.y + y;
+                    pos.z = 0;
+                    // Loop over all lava tiles
+                    if (tile == lavaTile)
+                    {
+                        // Nothing below, then flow downward
+                        if (!tilemap.HasTile(pos + Vector3Int.down))
+                        {
+                            tilemap.SetTile(pos + Vector3Int.down, lavaTile);
+                        }
+                        // Something is below and isn't lava
+                        else if (tilemap.GetTile(pos + Vector3Int.down) != lavaTile)
+                        {
+                            if (!tilemap.HasTile(pos + Vector3Int.left))
+                            {
+                                tilemap.SetTile(pos + Vector3Int.left, lavaTile);
+                            }
+                            if (!tilemap.HasTile(pos + Vector3Int.right))
+                            {
+                                tilemap.SetTile(pos + Vector3Int.right, lavaTile);
+                            }
+                        }
+                    }
+                }
+            }
+
+
+
+
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/Mark/TileClick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UI;
+
+public class TileClick : MonoBehaviour
+{
+    
+    public Tilemap tilemap;
+    
+    public TileBase swaptile;
+    public RuleTile ruleTile;
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 pos = Input.mousePosition;
+            Vector3Int tilePos = tilemap.WorldToCell(Camera.main.ScreenToWorldPoint(pos));
+            tilemap.SetTile(tilePos, ruleTile);
+            //tilemap.RefreshAllTiles();
+        }
+
+    }
+
+}
