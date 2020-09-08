149748f14c510756ce5469523759fcf4446f9923
Daniel Park <luckynumberseven07@gmail.com>
2020-09-08 15:38:29 -0700

    Small fixes, Bamboo Shoots can now be picked up

--- Assets/Scripts/Daniel/Movement.cs
+++ Assets/Scripts/Daniel/Movement.cs
@@ -113,8 +113,8 @@ public class Movement : MonoBehaviour
 
     public bool CheckInFront()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, DirectionToVector(faceDirection), 1f, LayerMask.GetMask("Obstacle", "Item", "NPC", "Puddle"));
-        if (hit.collider != null)
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, DirectionToVector(faceDirection), 1f, LayerMask.GetMask("Obstacle", "Item", "NPC", "Puddle", "Lava"));
+        if (hit.collider != null && !hit.collider.CompareTag("walkable"))
             return false;
         return true;
     }
