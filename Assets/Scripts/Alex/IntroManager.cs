760ae0a8c8c1567af0344b083e41d3a3afacdab3
Daniel Park <luckynumberseven07@gmail.com>
2020-09-12 00:48:03 -0700

    Fixed typo, gave clues for volcano level, can't pick up the chicken anymore

--- Assets/Scripts/Alex/IntroManager.cs
+++ Assets/Scripts/Alex/IntroManager.cs
@@ -16,9 +16,9 @@ public class IntroManager : MonoBehaviour
         List<string> sentences = new List<string>();
         sentences.Add("Long ago, in a distant island, known as the land of the rising sun, there lived a humble broth maker.");
         sentences.Add("Dedicated to the culinary craft of broth making, the man sought to create the ultimate recipe...");
-        sentences.Add("...a masterpiece extending numerous generations, a meal people would forever remember and enjoy - especially poor with no food.");
+        sentences.Add("...a masterpiece extending numerous generations, a meal people would forever remember and enjoy - especially the poor with no food.");
         sentences.Add("Unfortunately, a lack of ingredients hindered his bowl of dreams.");
-        sentences.Add("The barren island could’t support a multitude of livestock, plants, and spices.");
+        sentences.Add("The barren island couldn’t support a multitude of livestock, plants, and spices.");
         sentences.Add("UNTIL… the man discovered an anomaly on his island - a time anomaly.");
         sentences.Add("Utilizing the powers of science, he reworked the 4th dimension in order to achieve his real goal - soup cooking.");
         sentences.Add("Equipped with his chronobombs, the chef manipulated time to obtain his ingredients, becoming a CHRONO CHEF.");
